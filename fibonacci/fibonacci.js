46f96bc012c5de48d8a74d23c7925ce226c0b441
James Baillie <jfbaillie4@gmail.com>
2019-01-06 13:53:58 -0800

    completed fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    } else {
+        let current = 1;
+        let last = 0;
+        for (i=1; i < number; i++) {
+            next_local = current + last;
+            last = current;
+            current = next_local;
+        };
+        return current;
+    }
 }
 
 module.exports = fibonacci
