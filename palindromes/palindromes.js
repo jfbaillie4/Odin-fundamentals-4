463c3f968991b8280ba9c570c3a902f6d438d7ba
James Baillie <jfbaillie4@gmail.com>
2019-01-05 18:44:01 -0800

    completed palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
-
-}
+function dePuncAndTrim (str) {
+    var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    var spaceRE = /\s/g;
+    let str2 = str.replace(punctRE, '').replace(spaceRE, '');
+    str2 = str2.toLowerCase()
+    return str2;
+ }
+ 
+ const palindromes = function(word) {
+    const cleaned = dePuncAndTrim(word);
+    const array = cleaned.split("");
+    const newString = array.reverse().join("");
+    return (cleaned === newString ? true : false);
+};
 
 module.exports = palindromes
